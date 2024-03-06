bf4ad1824bb3ab47bb08e874d050e04a8ac9f6e0
SuperKiment <kiment2002@gmail.com>
2024-03-06 11:19:04 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MyTabs from './components/Bottom-Tabs';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MyTabs from "./components/Bottom-Tabs";
+import { UserProvider } from "./components/UserConnexion";
 
 export default function App() {
   return (
-    <MyTabs>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </MyTabs>
+    <UserProvider>
+      <MyTabs>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </MyTabs>
+    </UserProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
