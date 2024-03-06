1fc77f0226048886e548eb4aa3a41b93ea999fff
SuperKiment <kiment2002@gmail.com>
2024-03-06 09:06:31 +0100

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,28 @@
-import { useState } from "react"
-import { View, Text } from "react-native"
-import Header from '../components/Header';
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import Header from "../components/Header";
 
-export default ()=> {
-    [variable, setVariable] = useState(0)
+export default () => {
+  [variable, setVariable] = useState(0);
 
-    return (
-        <View>
-            <Header />
-            <Text>Coucouu Hello</Text>
-            <Text>{variable}</Text>
-        </View>
-    )
-}
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://s4-8060.nuage-peda.fr/ShareMelanie/Share/public/api"
+    );
+    const dataJSON = await data.json();
+
+    setVariable(dataJSON);
+  };
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  return (
+    <View>
+      <Header />
+      <Text>Coucouu Hello</Text>
+      <Text>{variable.test}</Text>
+    </View>
+  );
+};
