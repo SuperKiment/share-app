8c2ec4b83588f6e6d8789dbcfe16aebef7ac3f12
Origine0 <antoine244smite@gmail.com>
2024-03-29 15:40:51 +0100

    [feat] Download test

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,34 +2,47 @@ import { useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import styles from "../Styles/styles";
 import Header from "../components/Header";
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
 
 export default () => {
-  [variable, setVariable] = useState(0);
+  const [downloaded, setDownloaded] = useState(null);
+
+  const downloadAndSaveFile = () => {
+    const downloadUri = "https://s4-8058.nuage-peda.fr/share-melanie/Share/uploads/fichiers/";
+  
+    fetch(`https://s4-8058.nuage-peda.fr/share-melanie/Share/public/api-getcheminfichier?id=${id}`)
+      .then(response => response.json())
+      .then(data => {
+        const fileUri = data.data.nom_serveur;
+        const downloadUrl = downloadUri + fileUri;
+        console.log(downloadUrl);
+        const destinationUri = `${FileSystem.cacheDirectory}file.jpg`;
+  
+        return FileSystem.downloadAsync(downloadUrl, destinationUri);
+      })
+      .then(({ uri }) => {
+        console.log('File downloaded to:', uri);
+        setDownloaded(true);
+        return Sharing.shareAsync(uri);
+      })
+      .catch(error => {
+        console.error('Error downloading file:', error);
+      });
+  };
 
   return (
-    <View style={styles.body}>
-      <Header />
-      <View style={styles.container}>
-        <Text style={styles.titre}>Accueil</Text>
-        <Text style={styles.description}>
-          Une application mobile révolutionnaire conçue pour{" "}
-          <Text style={[styles.gras, styles.italic]}>simplifier le partage</Text> de fichiers
-          entre utilisateurs Android.
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Download File Example</Text>
+      <TouchableOpacity onPress={getFichiersById}>
+        <Text>Download File</Text>
+      </TouchableOpacity>
+      
+      {downloaded && (
+        <Text>
+          Téléchargé!
         </Text>
-        <Image
-          style={styles.image}
-          source={require("../assets/imageAccueil.jpg")}
-        />
-        <Text style={styles.description}>
-          Avec une interface conviviale et des fonctionnalités avancées, "Share"
-          vise à faciliter{" "}
-          <Text style={[styles.gras, styles.italic]}>l'échange de données numériques</Text> de
-          manière sécurisée et efficace.
-        </Text>
-        <TouchableOpacity style={styles.bouton}>
-          <Text style={styles.texteBouton}>Partager</Text>
-        </TouchableOpacity>
-      </View>
+      )}
     </View>
   );
-};
+}
