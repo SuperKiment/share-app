46514df97cf8654ee46f7f460cbbb83ff12f6b65
Mélanie <117924021+MelB04@users.noreply.github.com>
2024-04-11 15:28:28 +0200

    feat:logo

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet } from 'react-native';
-import styles from '../Styles/styles';
+import React from "react";
+import { SafeAreaView, View, Text, StyleSheet, Image } from "react-native";
+import styles from "../Styles/styles";
 
 const Header = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-    <View style={styles.header}>
-      <Text style={styles.headerText}>Share</Text>
-    </View>
+      <View style={styles.header}>
+        <Image source={require("../assets/shareLogo.png")} />
+
+        <Text style={styles.headerText}>Share</Text>
+      </View>
     </SafeAreaView>
   );
 };
