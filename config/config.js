c839cb44e5b50c9a530b6748a9ba03791de41b6a
SuperKiment <kiment2002@gmail.com>
2024-05-29 01:52:06 +0200

    export

---
+++ config/config.js
@@ -0,0 +1,5 @@
+//Tu réalises une copie de ce fichier dans ce dossier que tu nommes "config.js" 
+//Tu remplaces avec l'url de ton Nuage (sans le /api)
+//BIEN PENSER A LAISSER LE "/" A LA FIN DU LIEN
+export const nuage = "https://s4-8057.nuage-peda.fr/share/";
+export const IRInuage = "/share/"; //Ne pas oublier les "/" et si c'est /ShareFinal/Share/ il faut bien mettre les 2
